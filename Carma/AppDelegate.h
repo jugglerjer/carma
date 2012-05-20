3954868c442643d3e2be9621f7c84b15de28fc77
Jeremy Lubin <lubin.jeremy@gmail.com>
2012-05-20 09:46:45 -0700

    Initial Commit

---
+++ Carma/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Carma
+//
+//  Created by Jeremy Lubin on 5/20/12.
+//  Copyright (c) 2012 Lubin Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Carma/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Carma
+//
+//  Created by Jeremy Lubin on 5/20/12.
+//  Copyright (c) 2012 Lubin Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
